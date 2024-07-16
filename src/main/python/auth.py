df5575a3abf8f947a17877feb34ef3f34e43f855
saddydead1 <luckybeyhuk@gmail.com>
2024-07-16 07:20:24 +0300

    🔥dialoger-auth

---
+++ src/main/python/auth.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from database import DataBase
+
+
+security = HTTPBasic()
+
+def verify(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    if not DataBase().get_user(credentials.username, credentials.password):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    else:
+        return credentials.username
+    
+def register(username: str, password: str):
+    if not DataBase().register_user(username, password):
+        raise HTTPException(
+            status_code=401,
+            detail="Duplicate username",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    else:
+        return True
